1d70663e4577fa7eaf615fd5451941634b1dd475
Aziz Kemal HOSCAN <kemal.hoscan@yahoo.com>
2020-02-06 17:32:54 +0000

    addPhraseToDisplay class method implemented again

--- js/Phrase.js
+++ js/Phrase.js
@@ -13,37 +13,21 @@
   and created list items relatively
 */
 
-addPhraseToDisplay() {
-    for (let i = 0; i < this.phrase.length; i++) {
-      var li = document.createElement("li");
+
+
+  addPhraseToDisplay(){
+
+    for(let i=0; i < this.phrase.length; i++){
+      let li = document.createElement('LI');
       li.innerHTML = this.phrase[i];
-      if (this.phrase[i] === " ") {
-        li.className = "space";
-        //else class name is hide letter because it must be a letter if it is not a space,
-        //also adding this.phrase[i] to add the letter type to the end of the class name
-      } else {
-        li.className = "hide letter " + this.phrase[i];
+      if(this.phrase[i].match(/[a-z]/i)){
+        li.classList.add(`hide`, `letter`, `${this.phrase[i]}`);
+      } else if (this.phrase[i].match(/\s/)) {
+        li.classList.add("space");
       }
-      //li element is appended to ul which already exists and has the id phrase
+      document.querySelector('#phrase ul').appendChild(li);
     }
-      document.querySelector('#phrase ul')
-        .appendChild(li);
   }
-
-
-  // addPhraseToDisplay(){
-
-  //   for(let i=0; i < this.phrase.length; i++){
-  //     let li = document.createElement('LI');
-  //     li.innerHTML = this.phrase[i];
-  //     if(this.phrase[i].match(/[a-z]/i)){
-  //       li.classList.add(`hide`, `letter`, `${this.phrase[i]}`);
-  //     } else if (this.phrase[i].match(/\s/)) {
-  //       li.classList.add("space");
-  //     }
-  //     document.querySelector('#phrase ul').appendChild(li);
-  //   }
-  // }
 
 
   checkLetter(char){
